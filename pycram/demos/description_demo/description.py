84b4b9b52b81747bcbe4d1ec16f9c13283a69bb2
makruse <makruse@uni-bremen.de>
2025-12-22 20:54:34 +0100

    updated description and base demo file

---
+++ pycram/demos/description_demo/description.py
@@ -0,0 +1,7 @@
+from pycram.robot_plans import *
+from pycram.external_interfaces import *
+import rclpy
+
+# from rclpy.qos import QoSProfile, ReliabilityPolicy
+from rclpy.publisher import Publisher
+from rclpy.node import Node
--- pycram/demos/talking_demo/GPSR.py
+++ pycram/demos/talking_demo/GPSR.py
@@ -1,8 +1,53 @@
+from pycram.external_interfaces import tmc
+
 # Describing
+    # TODO: FIX AND PERCEPTION
+
 # count
+    # go to
+    # percieve
+    # SIMILIAR to describe but with a count of entitys
+
 # Guide
+    # Ask what to do
+    # plan guiding
+    # guide
+    # end guide
+
 # pick up
+    # basic pickup TODO: rework the plan, if not already
+
 # bring to
+    # NLP Query
+    # drive to action
+    # percieve action
+    # Pickup action
+    # NLP- Query
+    # go to action
+    # place on action
+
 # deliver
+    # drive to action
+    # percieve action
+    # Pickup action
+    # go to action
+    # hand over action
+
 # tell
+talkingNode = tmc.TextToSpeechPublisher()
+
+# here must be the nlp pipeline implemented TODO
+
+person = "Timo"
+talkingNode.say(f"please tell me what you would like me to tell {person}" )
+
+# here must be the nlp pipeline implemented TODO
+
 # OPTIONAL: FInd
+
+def goBackToRefereePose(self):
+    # Percieve/find waving human
+
+    # go to waving human with discrependency
+        # Nav2 pose with calculation of difference
+    raise NotImplementedError("Referee pose cant be determined")
