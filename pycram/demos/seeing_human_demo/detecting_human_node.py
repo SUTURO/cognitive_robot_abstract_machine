43e6e5485a6a543c0f42a28045a4f7371a95b376
Franco <franco@groegl.de>
2026-01-07 15:13:09 +0100

    renaming robokudo to robokudo_ros1 and robokudo_v2 to robokudo

--- pycram/demos/seeing_human_demo/detecting_human_node.py
+++ pycram/demos/seeing_human_demo/detecting_human_node.py
@@ -1,11 +1,11 @@
 import rclpy
-from pycram.external_interfaces import robokudo_v2
+from pycram.external_interfaces import robokudo
 
 rclpy.init()
-location = robokudo_v2.query_human()
+location = robokudo.query_human()
 if location is None:
     print("Error or No Human seen")
 else:
     print(f"Human seen at: {location}")
-robokudo_v2.shutdown_robokudo_interface()
+robokudo.shutdown_robokudo_interface()
 rclpy.shutdown()
--- pycram/src/pycram/external_interfaces/robokudo.py
+++ pycram/src/pycram/external_interfaces/robokudo.py
@@ -1,96 +1,76 @@
-import sys
 import logging
-from threading import Lock, RLock
-from typing import Any
-
-from ..ros import create_action_client
-from ..ros import get_node_names
-
+import threading
+from threading import Thread
+from rclpy.action.client import ClientGoalHandle
+from rclpy.executors import MultiThreadedExecutor
+from rclpy.node import Node
+from rclpy.action import ActionClient
+from pycram.ros import create_action_client
+from robokudo_msgs.action import Query
+from robokudo_msgs.msg import ObjectDesignator as RobokudoObjectDesignator
 from typing_extensions import List, Callable, Optional
-
+from typing import Any
 from ..datastructures.pose import PoseStamped
 from ..designator import ObjectDesignatorDescription
 
 logger = logging.getLogger(__name__)
 
-robokudo_found = False
-
+# Global variables for shared resources
+robokudo_action_client: ActionClient | None = None
+robokudo_node: Node | None = None
 is_init = False
-client = None
-
-number_of_par_goals = 0
-robokudo_lock = Lock()
-robokudo_rlock = RLock()
-with robokudo_rlock:
-    par_threads = {}
-    par_motion_goal = {}
+current_goal_handle: ClientGoalHandle | None = None
+executor: MultiThreadedExecutor | None = None
+executor_thread: Thread | None = None
 
 
-def thread_safe(func: Callable) -> Callable:
-    """
-    Adds thread safety to a function via a decorator. This uses the robokudo_lock
+def create_robokudo_action_client():
+    """Creates a new ActionClient and a MultithreadedExecutor for the Robokudo interface."""
 
-    :param func: Function that should be thread safe
-    :return: A function with thread safety
-    """
+    global robokudo_node, executor, executor_thread
 
-    def wrapper(*args, **kwargs):
-        with robokudo_rlock:
-            return func(*args, **kwargs)
+    if robokudo_node is None:
+        robokudo_node = Node("robokudo_interface_client")
 
-    return wrapper
+    client = create_action_client("/robokudo/query", Query, robokudo_node)
+
+    if executor is None:
+        executor = MultiThreadedExecutor()
+        executor.add_node(robokudo_node)
+
+        executor_thread = Thread(target=executor.spin, daemon=True)
+        executor_thread.start()
+        logger.info("Started MultiThreadedExecutor")
+
+    if not client.wait_for_server(timeout_sec=10.0):
+        logger.error("robokudo_query action server not available")
+        return None
+
+    return client
 
 
 def init_robokudo_interface(func: Callable) -> Callable:
-    """
-    Checks if the ROS messages are available and if Robokudo is running, if that is the case the interface will be
-    initialized.
-
-    :param func: Function this decorator should be wrapping
-    :return: A callable function which initializes the interface and then calls the wrapped function
-    """
+    """Initializes the Robokudo interface"""
 
     def wrapper(*args, **kwargs):
+        global is_init, robokudo_action_client
 
-        from robokudo_msgs.msg import ObjectDesignator as robokudo_ObjectDesignator
-        from robokudo_msgs.msg import QueryAction, QueryGoal, QueryResult
-        from geometry_msgs.msg import PointStamped
-
-        robokudo_found = True
-
-        global is_init
-        global client
-        if is_init and "/robokudo" in get_node_names():
+        if is_init:
             return func(*args, **kwargs)
-        elif is_init and "/robokudo" not in get_node_names():
-            logger.warning(
-                "Robokudo node is not available anymore, could not initialize robokudo interface"
-            )
-            is_init = False
-            return
+        try:
+            robokudo_action_client = create_robokudo_action_client()
 
-        if "robokudo_msgs" not in sys.modules:
-            logger.warning(
-                "Could not initialize the Robokudo interface since the robokudo_msgs are not imported"
-            )
-            return
+            if robokudo_action_client is None:
+                logger.warning("Could not create robokudo action client")
+                return None
 
-        if "/robokudo" in get_node_names():
-            logger.info("Successfully initialized Robokudo interface")
+            logger.info("Successfully initialized robokudo interface")
             is_init = True
-            client = create_action_client("robokudo/query", QueryAction)
-            logger.info("Waiting for action server")
-            if client.wait_for_server():
-                logger.info("Action server is available")
-            else:
-                logger.warning("Action server is not available")
-                is_init = False
-                return
-        else:
-            logger.warning(
-                "Robokudo is not running, could not initialize Robokudo interface"
-            )
-            return
+
+        except Exception as e:
+            logger.error(f"Failed to initialize robokudo interface: {e}")
+            return None
+
         return func(*args, **kwargs)
 
     return wrapper
@@ -104,8 +84,8 @@ def send_query(
 ) -> Any:
     """Generic function to send a query to RoboKudo."""
 
-    global client
-    goal = QueryGoal()
+    global robokudo_action_client
+    goal = Query.Goal()
 
     if obj_type:
         goal.obj.type = obj_type
@@ -114,47 +94,29 @@ def send_query(
     if attributes:
         goal.obj.attribute = attributes
 
-    query_result = None
+    result: Query.Result | None = None
+    result_event = threading.Event()
 
-    def done_callback(state, result):
-        nonlocal query_result
-        query_result = result
-        logger.info("Query completed with state: %s" % state)
+    def goal_response_callback(future):
+        goal_handle = future.result()
+        if not goal_handle.accepted:
+            logger.info("Goal rejected")
+            return
+        logger.info("Goal accepted")
+        get_result_future = goal_handle.get_result_async()
+        get_result_future.add_done_callback(get_result_callback)
 
-    def active_callback():
-        logger.info("Goal is now being processed by the action server")
+    def get_result_callback(future):
+        nonlocal result
+        result = future.result().result
+        logger.info("Finished ")
+        result_event.set()
 
-    def feedback_callback(feedback):
-        logger.info("Received feedback: %s" % feedback)
+    logger.info("Send Query")
+    send_goal_future = robokudo_action_client.send_goal_async(goal)
+    send_goal_future.add_done_callback(goal_response_callback)
 
-    client.send_goal(
-        goal,
-        done_cb=done_callback,
-        active_cb=active_callback,
-        feedback_cb=feedback_callback,
-    )
-    logger.info("Goal has been sent to the action server")
-
-    client.wait_for_result()
-    logger.info("Waiting for result from the action server")
-    return query_result
-
-
-@init_robokudo_interface
-def query_all_objects() -> dict:
-    """Query RoboKudo for all objects."""
-    result = send_query()
-
-    return result
-
-
-@init_robokudo_interface
-def query_object(obj_desc: ObjectDesignatorDescription) -> dict:
-    """Query RoboKudo for an object that fits the description."""
-    goal = QueryGoal()
-    goal.obj.type = str(obj_desc.types[0])
-
-    result = send_query(obj_type=goal.obj.type)
+    result_event.wait()
 
     return result
 
@@ -169,28 +131,38 @@ def query_human() -> "PointStamped":
 
 
 @init_robokudo_interface
+def query_all_objects() -> dict:
+    return send_query()
+
+
+@init_robokudo_interface
+def query_object(obj_desc: ObjectDesignatorDescription) -> dict:
+    return send_query(obj_type=str(obj_desc.types[0]))
+
+
+"""
+Cancel needs to be implemented bevor this will work.
+@init_robokudo_interface
 def stop_query():
-    """Stop any ongoing query to RoboKudo."""
-    global client
-    client.cancel_all_goals()
-    info("Cancelled current RoboKudo query goal")
+    global robokudo_action_client
+    robokudo_action_client.cancel_goal_async()
+    logger.info("Cancelled current Robokudo query goal")
+
+"""
 
 
 @init_robokudo_interface
 def query_specific_region(region: str) -> Any:
-    """Query RoboKudo to scan a specific region."""
     return send_query(region=region)
 
 
 @init_robokudo_interface
 def query_human_attributes() -> Any:
-    """Query RoboKudo for human attributes like brightness of clothes, headgear, and gender."""
     return send_query(obj_type="human", attributes=["attributes"])
 
 
 @init_robokudo_interface
 def query_waving_human() -> PoseStamped:
-    """Query RoboKudo for detecting a waving human."""
     result = send_query(obj_type="human")
     if result and result.res:
         try:
@@ -199,3 +171,21 @@ def query_waving_human() -> PoseStamped:
         except IndexError:
             pass
     return None
+
+
+def shutdown_robokudo_interface():
+    """Clean shutdown of perception interface."""
+
+    global robokudo_node, executor, executor_thread, is_init
+
+    if executor is not None:
+        executor.shutdown()
+
+    if executor_thread is not None:
+        executor_thread.join(timeout=5)
+
+    if robokudo_node is not None:
+        robokudo_node.destroy_node()
+
+    is_init = False
+    logger.info("Navigation interface shut down")
