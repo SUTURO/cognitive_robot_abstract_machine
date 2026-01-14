e1a9f47b05004f88d2bc8e63a37740bcfbbb5802
Yannick Schroeder <yschroed@uni-bremen.de>
2026-01-14 13:53:04 +0100

    demo for sprint perception

--- pycram/demos/sprint01_demo/perc_demo.py
+++ pycram/demos/sprint01_demo/perc_demo.py
@@ -3,14 +3,21 @@ from pycram.external_interfaces import robokudo
 
 rclpy.init()
 result = robokudo.query_object_str("beverage")
+# result = robokudo.query_all_objects()
 processed_results = []
 if result and result.res:
     for obj in result.res:
         if obj.description and obj.pose:
             description = obj.description[0]
-            position = obj.pose[0].pose.position
-            processed_results.append({"description": description, "position": position})
+            if "colabottle" in description.lower():
+                position = obj.pose[0].pose.position
+                processed_results.append(
+                    {"description": description, "position": position}
+                )
 
-print(processed_results)
+if processed_results:
+    print(processed_results)
+else:
+    print("Das gewünschte Objekt wurde nicht gefunden.")
 robokudo.shutdown_robokudo_interface()
 rclpy.shutdown()
