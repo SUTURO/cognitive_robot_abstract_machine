79f5d790d2823e93318dd44e48319a36d5412e58
Marvin <marvin8@uni-bremen.de>
2026-01-14 12:44:31 +0100

    Open & close gripper

--- giskardpy/src/giskardpy/motion_statechart/goals/pick_up.py
+++ giskardpy/src/giskardpy/motion_statechart/goals/pick_up.py
@@ -1,23 +1,47 @@
 from dataclasses import dataclass, field
+from enum import Enum
 
 from giskardpy.motion_statechart.context import BuildContext
 from giskardpy.motion_statechart.goals.templates import Sequence
 from giskardpy.motion_statechart.graph_node import Goal, NodeArtifacts
 from giskardpy.motion_statechart.tasks.cartesian_tasks import CartesianPose
+from giskardpy.motion_statechart.tasks.joint_tasks import JointPositionList, JointState
 from pycram.datastructures.pose import PoseStamped
 from semantic_digital_twin.world_description.world_entity import (
     KinematicStructureEntity,
+    Body,
 )
+
+
+class HSRGripper(Enum):
+    open_gripper = 1.23
+    close_gripper = 0
 
 
 @dataclass(repr=False, eq=False)
 class PickUp(Goal):
+    root_link: KinematicStructureEntity = field(kw_only=True)
     tip_link: KinematicStructureEntity = field(kw_only=True)
+    object_geometry: Body = field(kw_only=True)
     ft: bool = field(kw_only=True, default=False)
 
     def expand(self, context: BuildContext) -> None:
         super().expand(context)
-        Sequence([PrePose, Grasping, CloseHand, PullUp])
+        Sequence([OpenHand, PrePose, Grasping, CloseHand, PullUp])
+
+    def build(self, context: BuildContext) -> NodeArtifacts:
+        return super().build(context)
+
+
+@dataclass(repr=False, eq=False)
+class OpenHand(Goal):
+    def expand(self, context: BuildContext) -> None:
+        joint_goal = JointPositionList(
+            goal_state=JointState.from_str_dict(
+                {"hand_motor_joint": HSRGripper.open_gripper.value}, context.world
+            )
+        )
+        self.add_node(joint_goal)
 
     def build(self, context: BuildContext) -> NodeArtifacts:
         return super().build(context)
@@ -30,6 +54,7 @@ class PrePose(Goal):
     def expand(self, context: BuildContext) -> None:
         super().expand(context)
 
+        # Orient to front facing axis of object
         pose = PoseStamped()
 
         pose = CartesianPose(
@@ -48,6 +73,7 @@ class Grasping(Goal):
 
     def expand(self, context: BuildContext) -> None:
         super().expand(context)
+        # Approach the object with the gripper
 
     def build(self, context: BuildContext) -> NodeArtifacts:
         return super().build(context)
@@ -59,7 +85,12 @@ class CloseHand(Goal):
     ft: bool = field(kw_only=True, default=False)
 
     def expand(self, context: BuildContext) -> None:
-        super().expand(context)
+        joint_goal = JointPositionList(
+            goal_state=JointState.from_str_dict(
+                {"hand_motor_joint": HSRGripper.close_gripper.value}, context.world
+            )
+        )
+        self.add_node(joint_goal)
 
     def build(self, context: BuildContext) -> NodeArtifacts:
         return super().build(context)
