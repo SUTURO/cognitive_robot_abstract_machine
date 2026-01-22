602e66257cb0b515896621c1c028bb8813260e35
Marvin <marvin8@uni-bremen.de>
2026-01-22 15:41:57 +0100

    code duplication

--- giskardpy/src/giskardpy/motion_statechart/goals/pick_up.py
+++ giskardpy/src/giskardpy/motion_statechart/goals/pick_up.py
@@ -1,12 +1,18 @@
+from abc import ABC, abstractmethod
 from dataclasses import dataclass, field
 from enum import Enum
-from typing import Optional
+from typing_extensions import Optional, List
 
 from giskardpy.data_types.exceptions import ForceTorqueSaysNoException
 from giskardpy.motion_statechart.context import BuildContext, ExecutionContext
 from giskardpy.motion_statechart.data_types import DefaultWeights
 from giskardpy.motion_statechart.goals.templates import Sequence, Parallel
-from giskardpy.motion_statechart.graph_node import Goal, NodeArtifacts, CancelMotion
+from giskardpy.motion_statechart.graph_node import (
+    Goal,
+    NodeArtifacts,
+    CancelMotion,
+    MotionStatechartNode,
+)
 from giskardpy.motion_statechart.ros2_nodes.force_torque_monitor import (
     ForceImpactMonitor,
 )
@@ -17,8 +23,12 @@ from giskardpy.motion_statechart.tasks.cartesian_tasks import (
 from giskardpy.motion_statechart.tasks.joint_tasks import JointPositionList, JointState
 from giskardpy.motion_statechart.tasks.pointing import Pointing
 from giskardpy.motion_statechart.test_nodes.test_nodes import ConstTrueNode
-from krrood.symbolic_math.symbolic_math import trinary_logic_not, trinary_logic_and
-from semantic_digital_twin.robots.abstract_robot import Manipulator
+from krrood.symbolic_math.symbolic_math import (
+    trinary_logic_not,
+    trinary_logic_and,
+    Scalar,
+)
+from semantic_digital_twin.robots.abstract_robot import Manipulator, ParallelGripper
 from semantic_digital_twin.spatial_types import (
     Vector3,
     Point3,
@@ -39,13 +49,16 @@ PICKUP_PREPOSE_DISTANCE = 0.03
 HSR_GRIPPER_WIDTH = 0.15
 PULLUP_HEIGHT = 0.1
 
+VERTICAL_DOT_THRESH = 0.85  # dot with world Z considered vertical
+HORIZONTAL_DOT_THRESH = 0.25  # dot with world Z considered horizontal
+
 
 @dataclass(repr=False, eq=False)
 class PickUp(Goal):
     # root_link: KinematicStructureEntity = field(kw_only=True)
     # NOTE: Pickup should be called split, meaning grabbing first and then separately retracting
     # because after grasping the object it should get attached to the tool frame in semdt
-    manipulator: Manipulator = field(kw_only=True)
+    manipulator: Manipulator = field(kw_only=True)  # TODO ParallelGripper instead
     object_geometry: Body = field(kw_only=True)
     ft: bool = field(kw_only=True, default=False)
 
@@ -97,17 +110,42 @@ class OpenHand(Goal):
 
 
 @dataclass(repr=False, eq=False)
-class PreGraspPose(Goal):
+class GraspMagic(ABC):
     manipulator: Manipulator = field(kw_only=True)
     object_geometry: Body = field(kw_only=True)
     gripper_width: float = field(kw_only=True)
     prefer_front_grasp: bool = field(default=False, kw_only=True)
     gripper_vertical: Optional[bool] = field(default=True, kw_only=True)
 
-    def expand(self, context: BuildContext) -> None:
-        if not self.manipulator or not self.object_geometry:
-            raise Exception("Bad input of PrePose Class")
+    @abstractmethod
+    def get_pre_grasp(self) -> List[MotionStatechartNode]:
+        pass
 
+    @abstractmethod
+    def get_grasp_nodes(self) -> List[MotionStatechartNode]:
+        """
+        Given an object geometry, returns a list of motion statechart nodes that grasp the object.
+        :return:
+        """
+
+
+@dataclass(repr=False, eq=False)
+class BoxGraspMagic(GraspMagic):
+    def get_pre_grasp(self) -> List[MotionStatechartNode]:
+        pass
+
+    def get_grasp_nodes(self) -> List[MotionStatechartNode]:
+        pass
+
+
+@dataclass(repr=False, eq=False)
+class PreGraspPose(Goal):
+    manipulator: Manipulator = field(kw_only=True)
+    object_geometry: Body = field(kw_only=True)
+    gripper_width: float = field(kw_only=True)
+    gripper_vertical: Optional[bool] = field(default=True, kw_only=True)
+
+    def expand(self, context: BuildContext) -> None:
         obj_pose = self.object_geometry.global_pose
         tool_frame = self.manipulator.tool_frame
         front_facing = self.manipulator.front_facing_axis
@@ -118,23 +156,12 @@ class PreGraspPose(Goal):
             self.object_geometry
         ).bounding_box()
 
-        # Get object dimensions
-        dims = [obj_bbox.depth, obj_bbox.width, obj_bbox.height]
-        if min(dims) > self.gripper_width:
-            raise Exception(
-                f"Object too large for gripper. Min dimension of object: {min(dims)}, gripper width: {self.gripper_width}"
-            )
-
-        # Calculate pre-grasp position on robot->object vector
         robot_pos = self.manipulator.tool_frame.global_pose
-        obj_to_robot = Vector3()
-        obj_to_robot.x = robot_pos.x - obj_pose.x
-        obj_to_robot.y = robot_pos.y - obj_pose.y
-        obj_to_robot.z = robot_pos.z - obj_pose.z
+        obj_to_robot = robot_pos.to_position() - obj_pose.to_position()
         obj_to_robot.norm()
         print(f"Object->robot vector on expand: {obj_to_robot}")
 
-        # TODO consider gripper orientation
+        # According to perception we can assume that the z axis points up
         faces = [
             # (axis_vector, (face_dim1, face_dim2))
             (
@@ -151,55 +178,33 @@ class PreGraspPose(Goal):
             ),  # approach along Z -> face is XxY
         ]
 
-        # Determine grasp direction
-        if self.prefer_front_grasp and (
-            obj_bbox.width <= self.gripper_width
-            or obj_bbox.height <= self.gripper_width
-        ):
-            grasp_axis = Vector3.X(self.object_geometry)
-        else:
-            valid_faces = [
-                (v, (d1, d2))
-                for v, (d1, d2) in faces
-                if min(d1, d2) <= self.gripper_width
-            ]
-            if not valid_faces:
-                raise Exception(
-                    "No valid grasp face found (no face has a face-dimension <= gripper width)."
-                )
-            grasp_axis = max(valid_faces, key=lambda x: abs(x[0].dot(obj_to_robot)))[0]
+        valid_faces = [
+            (v, (d1, d2)) for v, (d1, d2) in faces if min(d1, d2) <= self.gripper_width
+        ]
+        if not valid_faces:
+            raise Exception(
+                "No valid grasp face found (no face has a face-dimension <= gripper width)."
+            )
+        grasp_axis = max(valid_faces, key=lambda x: abs(x[0].dot(obj_to_robot)))[0]
 
         grasp_axis.norm()
         print(f"Grasp axis: {grasp_axis.to_np()}")
 
-        axis_np = grasp_axis.to_np()
-        dot_along = (
-            axis_np[0] * obj_to_robot.x
-            + axis_np[1] * obj_to_robot.y
-            + axis_np[2] * obj_to_robot.z
-        )
+        dot_along = grasp_axis.dot(obj_to_robot)
+        sign = 1.0 if dot_along >= Scalar(0.0) else -1.0
 
-        sign = 1.0 if dot_along >= 0.0 else -1.0
-
-        if abs(axis_np[0]) > 0.9:
+        if abs(grasp_axis.x) > 0.9:
             half_extent = obj_bbox.depth / 2.0
-        elif abs(axis_np[1]) > 0.9:
+        elif abs(grasp_axis.y) > 0.9:
             half_extent = obj_bbox.width / 2.0
         else:
             half_extent = obj_bbox.height / 2.0
 
         offset_distance = half_extent + PICKUP_PREPOSE_DISTANCE
+        offset_vector = grasp_axis * (offset_distance * sign)
 
-        pre_grasp_point = Point3()
-        pre_grasp_point.x = obj_pose.x + (axis_np[0] * offset_distance * sign)
-        pre_grasp_point.y = obj_pose.y + (axis_np[1] * offset_distance * sign)
-        pre_grasp_point.z = obj_pose.z + (axis_np[2] * offset_distance * sign)
+        pre_grasp_point = obj_pose.to_position() + offset_vector
         pre_grasp_point.reference_frame = context.world.root
-
-        # print(f'Object point: x={obj_pose.x}, y={obj_pose.y}, z={obj_pose.z}')
-        # print(f'Pre grasp point: x={pre_grasp_point.x}, y={pre_grasp_point.y}, z={pre_grasp_point.z}')
-        # print(f'Object bounding box origin: {obj_bbox.origin}')
-        # print(f'Object bounding box: width={obj_bbox.width}, height={obj_bbox.height}, depth={obj_bbox.depth}')
 
         self._cart_pose = CartesianPosition(
             root_link=context.world.root,
@@ -276,7 +281,7 @@ class Grasping(Goal):
     object_geometry: Body = field(kw_only=True)
 
     def expand(self, context: BuildContext) -> None:
-        # TODO implement better grasping motion
+        # TODO implement better grasping motion w feedback
         # TODO rim offset: object_dimension_in_approach_direction / 2
         goal_point = Point3()
         goal_point.x = self.object_geometry.global_pose.x
@@ -353,64 +358,4 @@ class PullUp(Goal):
             )
         else:
             artifacts.observation = self._cart_position.observation_variable
-        return artifacts
-
-
-@dataclass(repr=False, eq=False)
-class Retracting(Goal):
-    """
-    Retracts the tool frame of a manipulator by a certain distance.
-    """
-
-    manipulator: Manipulator = field(kw_only=True)
-    distance: float = field(default=0.15, kw_only=True)
-    reference_frame: Optional[KinematicStructureEntity] = field(
-        default=None, kw_only=True
-    )
-    velocity: float = field(default=0.1, kw_only=True)
-    weight: float = field(default=DefaultWeights.WEIGHT_ABOVE_CA, kw_only=True)
-
-    def expand(self, context: BuildContext) -> None:
-        tip_link = self.manipulator.tool_frame
-        root_link = context.world.root
-
-        goal_point = self.manipulator.tool_frame.global_pose.to_position()
-        goal_point.z += PULLUP_HEIGHT
-        # goal_point = Point3(0, 0, 0, reference_frame=tip_link)
-        # goal_point = context.world.transform(
-        #     target_frame=ref_frame, spatial_object=goal_point
-        # )
-        # goal_point.x += direction.x
-        # goal_point.y += direction.y
-        # goal_point.z += direction.z
-
-        # world_goal_point = context.world.transform(
-        #     target_frame=root_link, spatial_object=goal_point
-        # )
-
-        self.cart_pos = CartesianPosition(
-            root_link=root_link,
-            tip_link=tip_link,
-            goal_point=goal_point,
-            weight=self.weight,
-        )
-
-        # self.keep_ori = AlignPlanes(
-        #     root_link=root_link,
-        #     tip_link=tip_link,
-        #     tip_normal=Vector3.Z(tip_link),
-        #     goal_normal=context.world.transform(
-        #         target_frame=root_link, spatial_object=Vector3.Z(tip_link)
-        #     ),
-        #     weight=self.weight,
-        # )
-
-        # self.add_node(parallel := Parallel([self.cart_pos, self.keep_ori]))
-        # self.parallel = parallel
-        self.add_node(self.cart_pos)
-
-    def build(self, context: BuildContext) -> NodeArtifacts:
-        artifacts = super().build(context)
-        # artifacts.observation = self.parallel.observation_variable
-        artifacts.observation = self.cart_pos.observation_variable
         return artifacts
--- giskardpy/src/giskardpy/motion_statechart/ros2_nodes/gripper_command.py
+++ giskardpy/src/giskardpy/motion_statechart/ros2_nodes/gripper_command.py
@@ -5,13 +5,15 @@ from giskardpy.motion_statechart.context import BuildContext, ExecutionContext
 from giskardpy.motion_statechart.data_types import ObservationStateValues
 from giskardpy.motion_statechart.graph_node import NodeArtifacts
 from giskardpy.motion_statechart.ros2_nodes.ros_tasks import ActionServerTask
+from typing_extensions import Type, TypeVar
 from tmc_control_msgs.action import GripperApplyEffort
 
 logger = logging.getLogger(__name__)
+Action = TypeVar("Action")
 
 
 @dataclass(eq=False, repr=False)
-class GripperCommand(
+class GripperCommandTask(
     ActionServerTask[
         GripperApplyEffort,
         GripperApplyEffort.Goal,
@@ -21,6 +23,7 @@ class GripperCommand(
 ):
 
     effort: float = field(kw_only=True)
+    message_type: Type[Action] = field(kw_only=True, default=GripperApplyEffort)
 
     def build_msg(self, context: BuildContext):
         goal_msg = GripperApplyEffort.Goal()
