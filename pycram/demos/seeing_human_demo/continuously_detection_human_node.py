2123b1a2c199246562d6a76073957335f30e4ccc
Franco <franco@groegl.de>
2026-02-05 14:09:09 +0100

    Anpassung an branch franco

--- pycram/demos/seeing_human_demo/continuously_detection_human_node.py
+++ pycram/demos/seeing_human_demo/continuously_detection_human_node.py
@@ -1,44 +1,37 @@
-import json
-
 import rclpy
-from rclpy.node import Node
-from std_msgs.msg import String
+from pycram.robot_plans import LookAtAction
+
+from pycram.language import SequentialPlan
+
 from pycram.external_interfaces import robokudo
+import time
 from time import sleep
-
-
-class CONTINUOUS_HUMAN_DETECTION(Node):
-    def __init__(self):
-
-        super().__init__("continous_human_detection")
-
-        self.sub_chd = self.create_subscription(
-            String, "/human_pose", self._data_callback, 10
-        )
-
-    def _data_callback(self, data):
-        print(data)
-        # self.parse_json_string(data.data)
-
-    def parse_json_string(self, json_string: str):
-        print(json_string)
-        try:
-            parsed = json.loads(json_string)
-            point = parsed.get("point")
-        except () as e:
-            print(f"Error parsing string:{e}")
-
-
-# ----------------------------------------------------------------
+from pycram.ros_utils.text_to_image import TextToImagePublisher
 
 
 def main():
     """Testing"""
-    rclpy.init()
-    robokudo.send_query("human")
-    chd = CONTINUOUS_HUMAN_DETECTION()
-    sleep(10)
-    robokudo.cancel_goal()
-    chd.destroy_node()
+    text_pub = TextToImagePublisher()
+    found_position = True
+    timeout = 15
+    # While human is seen print location
+    while found_position:
+        # Send goal
+        position = robokudo.query_current_human_position_in_continues()
+        if position is not None and position.header.stamp.sec > time.time() - timeout:
+            x = round(position.point.x)
+            y = round(position.point.y)
+            z = round(position.point.z)
+            text_pub.publish_text(f"Point x: {x} y: {y} z: {z}")
+        else:
+            text_pub.publish_text("No Human seen.")
+            found_position = False
+        sleep(0.5)
+
+    # Close every think
     robokudo.shutdown_robokudo_interface()
     rclpy.shutdown()
+
+
+if __name__ == "__main__":
+    main()
