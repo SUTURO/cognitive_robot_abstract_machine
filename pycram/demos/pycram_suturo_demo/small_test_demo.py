64f2e4d6ea91667f11e20355532bb6983a862855
Merckse <marc.kruse.2003@gmail.com>
2026-02-20 04:29:48 +0100

    moving and minor fixes, not tested yet

--- pycram/demos/pycram_suturo_demo/small_test_demo.py
+++ pycram/demos/pycram_suturo_demo/small_test_demo.py
@@ -1,8 +1,8 @@
 import rclpy
 
+from demos.helper_methods_and_useful_classes.robot_setup import robot_setup
 from pycram.datastructures.enums import Arms
 from pycram.datastructures.pose import PoseStamped
-from pycram_suturo_demo.setup_real_robot import world_setup_with_test_objects
 from pycram.language import SequentialPlan
 from pycram.motion_executor import real_robot
 from pycram.robot_plans import (
@@ -11,7 +11,7 @@ from pycram.robot_plans import (
 )
 
 rclpy.init()
-result = world_setup_with_test_objects()
+result = robot_setup()
 world, robot_view, context = (
     result.world,
     result.robot_view,
--- pycram/demos/robot_setup.py
+++
@@ -1,8 +0,0 @@
-from helper_methods_and_useful_classes import real_setup
-from helper_methods_and_useful_classes.simulation_setup import setup_hsrb_in_environment
-from suturo_resources.suturo_map import load_environment
-
-
-def robot_setup(simulation: bool = True, with_objects: bool = True):
-    return setup_hsrb_in_environment(load_environment=load_environment(), with_viz=True, with_obj=with_objects
-                                     ) if simulation else real_setup.world_setup_with_test_objects()
