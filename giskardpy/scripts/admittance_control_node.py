c3ad8637a9f125978fa272ddeb45c5fbe760b777
endxrpxl <ansgar.joerss@t-online.de>
2026-06-19 16:41:39 +0200

    change script

--- giskardpy/scripts/admittance_control_node.py
+++ giskardpy/scripts/admittance_control_node.py
@@ -1,26 +1,32 @@
+from giskardpy.middleware.ros2 import rospy
 from giskardpy.motion_statechart.graph_node import EndMotion
 from giskardpy.motion_statechart.monitors.monitors import LocalMinimumReached
 from giskardpy.motion_statechart.motion_statechart import MotionStatechart
-from giskardpy.motion_statechart.ros2_nodes.force_torque_monitor import ForceTorqueSymbolNode
-from giskardpy.motion_statechart.tasks.admittance_tasks import AdmittanceCartesianPosition
+from giskardpy.motion_statechart.ros2_nodes.force_torque_monitor import (
+    ForceTorqueSymbolNode,
+)
+from giskardpy.motion_statechart.tasks.admittance_tasks import (
+    AdmittanceCartesianPosition,
+)
 from giskardpy_ros.python_interface.python_interface import GiskardWrapper
-from giskardpy_ros.ros2 import rospy
 
 from semantic_digital_twin.spatial_types import Point3, Vector3
 
-rospy.init_node('admittance_demo')
+rospy.init_node("admittance_demo")
 
 giskard = GiskardWrapper(node_handle=rospy.node)
 
-tip = giskard.world.get_kinematic_structure_entity_by_name('hand_gripper_tool_frame')
+tip = giskard.world.get_kinematic_structure_entity_by_name("hand_gripper_tool_frame")
 root = giskard.world.root
-ft_sensor_frame = giskard.world.get_kinematic_structure_entity_by_name('wrist_ft_sensor_frame')
+ft_sensor_frame = giskard.world.get_kinematic_structure_entity_by_name(
+    "wrist_ft_sensor_frame"
+)
 
 goal = Point3(x=0.0, y=0.0, z=0.5, reference_frame=root)
 
 ft_node = ForceTorqueSymbolNode(
     reference_frame=ft_sensor_frame,
-    topic_name='/hsrb/wrist_wrench/compensated',
+    topic_name="/hsrb/wrist_wrench/compensated",
 )
 
 cart_goal = AdmittanceCartesianPosition(
@@ -28,7 +34,7 @@ cart_goal = AdmittanceCartesianPosition(
     tip_link=tip,
     goal_point=goal,
     ft_node=ft_node,
-    K=Vector3(50, 50, 50)
+    K=Vector3(50, 50, 50),
 )
 
 msc = MotionStatechart()
